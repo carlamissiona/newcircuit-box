89ea9524c13af7dc755aef9736241fce5c40ed89
missiona.carla <missiona.carla@gmail.com>
2025-06-12 03:10:14 +0000

    push

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3019;
+const PORT = process.env.PORT || 3020;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
